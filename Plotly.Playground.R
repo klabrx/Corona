38938fef8c8ad4d09d0e1e937e2c7c13505c7e06
klabrx <9hq3f45wp3@snkmail.com>
2021-05-12 04:53:46 +0200

    daily

--- Plotly.Playground.R
+++ Plotly.Playground.R
@@ -1,6 +1,8 @@
 library(plotly)
 timeline.plot <- dashdat.pivot %>%
   filter(grepl("0926.", AGS)) %>%
-  filter(Datum>= "2021-04-01") %>%
+  filter(Datum>= "2021-01-01") %>%
   plot_ly(x=~Datum, y=~Inzidenz, type='scatter', color=~Kreis, mode='lines')
 timeline.plot
+
+dashdat.pivot 
