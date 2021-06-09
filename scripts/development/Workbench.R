55ab9c2ad5bf5246a5b3ba8f37006fc03c773e64
klabrx <9hq3f45wp3@snkmail.com>
2021-06-09 04:25:24 +0200

    backup: sankey

--- scripts/development/Workbench.R
+++ scripts/development/Workbench.R
@@ -124,3 +124,42 @@ dimnames(m) <- list(have = Stadtteile,
 groupColors <- viridis_pal(option = "C")(16)
 chorddiag(m, groupColors = groupColors, groupnamePadding = 25)
 
+
+# sankey -----------------------------------------------------------------------
+# Load package
+library(networkD3)
+
+# Load energy projection data
+URL <- "https://cdn.rawgit.com/christophergandrud/networkD3/master/JSONdata/energy.json"
+Energy <- jsonlite::fromJSON(URL)
+
+
+# Now we have 2 data frames: a 'links' data frame with 3 columns (from, to, value), and a 'nodes' data frame that gives the name of each node.
+head( Energy$links )
+head( Energy$nodes )
+
+# Thus we can plot it
+p <- sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
+                   Target = "target", Value = "value", NodeID = "name",
+                   units = "TWh", fontSize = 12, nodeWidth = 30)
+p
+
+# save the widget
+# library(htmlwidgets)
+
+
+# collapsible tree -------------------------------------------------------------
+# Load library
+# install.packages("collapsibleTree")
+library(collapsibleTree) 
+
+# input data must be a nested data frame:
+head(warpbreaks)
+
+# Represent this tree:
+p <- collapsibleTree( warpbreaks, c("wool", "tension", "breaks"))
+p
+
+# save the widget
+# library(htmlwidgets)
+# saveWidget(p, file=paste0( getwd(), "/HtmlWidget/dendrogram_interactive.html"))
